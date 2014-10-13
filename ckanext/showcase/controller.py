0b178ac284b54747d15c51fa99c538f2c833c439
Brook Elgie <specialbrew@gmail.com>
2014-10-13 11:34:07 +0100

    Show & Tell tab in user dashboard templates

---
+++ ckanext/showcase/controller.py
@@ -0,0 +1,19 @@
+import ckan.lib.base as base
+from ckan.common import c
+from ckan.controllers.user import UserController
+
+render = base.render
+
+import logging
+log = logging.getLogger(__name__)
+
+
+class ShowcaseDashboardController(UserController):
+
+    def dashboard_showandtell(self):
+        '''Displays all Show & Tell items created by the user.'''
+        context = {'for_view': True, 'user': c.user or c.author,
+                   'auth_user_obj': c.userobj}
+        data_dict = {'user_obj': c.userobj}
+        self._setup_template_variables(context, data_dict)
+        return render('user/dashboard_showandtell.html')
--- ckanext/showcase/plugin.py
+++ ckanext/showcase/plugin.py
@@ -1,13 +1,32 @@
+import logging
+
 import ckan.plugins as plugins
+import ckan.lib.plugins as lib_plugins
 import ckan.plugins.toolkit as toolkit
 
+log = logging.getLogger(__name__)
 
-class ShowcasePlugin(plugins.SingletonPlugin):
+
+class ShowcasePlugin(plugins.SingletonPlugin, lib_plugins.DefaultGroupForm):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IGroupForm)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'showcase')
+        # toolkit.add_public_directory(config_, 'public')
+        # toolkit.add_resource('fanstatic', 'showcase')
+
+    def group_types(self):
+        return ["showing"]
+
+    def is_fallback(self):
+        return False
+
+    def before_map(self, map):
+        map.connect('user_dashboard_showandtell', '/dashboard/showandtell',
+                    controller='ckanext.showcase.controller:ShowcaseDashboardController',
+                    action='dashboard_showandtell', ckan_icon="bullhorn")
+        return map
