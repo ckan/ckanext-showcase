0b2d41abaecfce8497496effb757c5a271f00f61
Brook Elgie <specialbrew@gmail.com>
2014-10-20 16:17:12 +0100

    Showcase item pages

--- ckanext/showcase/plugin.py
+++ ckanext/showcase/plugin.py
@@ -4,6 +4,8 @@ import ckan.plugins as plugins
 import ckan.lib.plugins as lib_plugins
 import ckan.plugins.toolkit as toolkit
 
+from routes.mapper import SubMapper
+
 log = logging.getLogger(__name__)
 
 
@@ -12,10 +14,14 @@ class ShowcasePlugin(plugins.SingletonPlugin, lib_plugins.DefaultGroupForm):
     plugins.implements(plugins.IGroupForm)
     plugins.implements(plugins.IRoutes, inherit=True)
 
+    # IConfigurer
+
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         # toolkit.add_public_directory(config_, 'public')
         # toolkit.add_resource('fanstatic', 'showcase')
+
+    # IGroupForm
 
     def group_types(self):
         return ['showcase']
@@ -27,15 +33,47 @@ class ShowcasePlugin(plugins.SingletonPlugin, lib_plugins.DefaultGroupForm):
         return False
 
     def index_template(self):
-        """
-        Returns a string representing the location of the template to be
-        rendered for the index page
-        """
         return 'showcase/index.html'
 
+    def read_template(self):
+        return 'showcase/read.html'
+
+    def activity_template(self):
+        return 'showcase/activity_stream.html'
+
+    def about_template(self):
+        return 'showcase/about.html'
+
+    # IRoutes
+
     def before_map(self, map):
-        map.connect('showcase_index', '/showcase',
-                    controller='ckanext.showcase.controller:ShowcaseController',
-                    action='index')
-        map.redirect('/showcases', '/showcase')
+        # These named routes are used for custom group forms which will use the
+        # names below based on the group.type ('group' is the default type)
+        with SubMapper(map, controller='ckanext.showcase.controller:ShowcaseController') as m:
+            m.connect('showcase_index', '/showcase', action='index',
+                      highlight_actions='index search')
+            m.connect('showcase_list', '/showcase/list', action='list')
+            m.connect('showcase_new', '/showcase/new', action='new')
+            m.connect('showcase_action', '/showcase/{action}/{id}',
+                      requirements=dict(action='|'.join([
+                          'edit',
+                          'delete',
+                          'member_new',
+                          'member_delete',
+                          'history',
+                          'followers',
+                          'follow',
+                          'unfollow',
+                          'admins',
+                          'activity',
+                      ])))
+            m.connect('showcase_about', '/showcase/about/{id}', action='about',
+                      ckan_icon='info-sign'),
+            m.connect('showcase_edit', '/showcase/edit/{id}', action='edit',
+                      ckan_icon='edit')
+            m.connect('showcase_members', '/showcase/members/{id}', action='members',
+                      ckan_icon='showcase'),
+            m.connect('showcase_activity', '/showcase/activity/{id}/{offset}',
+                      action='activity', ckan_icon='time'),
+            m.connect('showcase_read', '/showcase/{id}', action='read', ckan_icon='sitemap')
         return map
