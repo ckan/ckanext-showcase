8bcfe0130e5a6234f4b76219d50cd849d37a9b23
Brook Elgie <specialbrew@gmail.com>
2014-10-30 12:42:28 +0000

    Fix tests

--- ckanext/showcase/tests/test_plugin.py
+++ ckanext/showcase/tests/test_plugin.py
@@ -1,6 +1,6 @@
-import mock
 from nose import tools as nosetools
 
+import ckan.lib.search as search
 import ckan.new_tests.helpers as helpers
 import ckan.new_tests.factories as factories
 
@@ -9,6 +9,11 @@ log = logging.getLogger(__name__)
 
 
 class TestShowcaseIndex(helpers.FunctionalTestBase):
+
+    def setup(self):
+        super(TestShowcaseIndex, self).setup()
+        # Clear the search index
+        search.clear()
 
     def test_loggedout_user_cannot_view_showcase(self):
         '''A not logged in user can view the Showcases page.'''
@@ -30,23 +35,22 @@ class TestShowcaseIndex(helpers.FunctionalTestBase):
         response = app.get('/showcases', status=302)
         nosetools.assert_equal(response.location, 'http://localhost/showcase')
 
-    # def test_showcases_redirects_to_showcase_for_item(self):
-    #     '''/showcases/ redirects to /showcase.'''
-    #     app = self._get_test_app()
-    #     response = app.get('/showcases', status=302)
-    #     nosetools.assert_equal(response.location, 'http://localhost/showcase')
+    def test_showcases_redirects_to_showcase_for_item(self):
+        '''/showcases/ redirects to /showcase.'''
+        app = self._get_test_app()
 
-    # @mock.patch('ckan.new_authz.auth_is_loggedin_user')
-    # def test_showcase_listed_on_index(self, mockauth):
-    #     '''
-    #     An added Showcase will appear on the Showcase index page.
-    #     '''
-    #     app = self._get_test_app()
+        factories.Dataset(type='showcase', name='my-showcase')
 
-    #     # Add the Showcase
-    #     mockauth.return_value = True
-    #     showcase_admin = factories.User()
-    #     showcase_group = factories.Group(type='showcase', user=showcase_admin)
+        response = app.get('/showcases/my-showcase', status=302)
+        nosetools.assert_equal(response.location, 'http://localhost/showcase/my-showcase')
 
-    #     response = app.get("/showcase", status=200)
-    #     response.mustcontain(showcase_group['name'], "1 showcase found")
+    def test_showcase_listed_on_index(self):
+        '''
+        An added Showcase will appear on the Showcase index page.
+        '''
+        app = self._get_test_app()
+
+        factories.Dataset(type='showcase')
+
+        response = app.get("/showcase", status=200)
+        response.mustcontain("1 showcase found")
