3c0b6bd8b2f2064c479612e456ea8d3b03d5b04d
Brook Elgie <specialbrew@gmail.com>
2014-11-09 16:14:12 +0000

    Reorder image upload w/r/t dataset create/update.
    The uploader changes the data_dict so the file name is correct when the
    package is saved, so this has to happen before the db commit.

--- ckanext/showcase/logic/action/update.py
+++ ckanext/showcase/logic/action/update.py
@@ -9,12 +9,12 @@ log = logging.getLogger(__name__)
 
 def showcase_update(context, data_dict):
 
-    pkg = toolkit.get_action('package_update')(context, data_dict)
-
     upload = uploader.Upload('showcase', data_dict['image_url'])
     upload.update_data_dict(data_dict, 'image_url',
                             'image_upload', 'clear_upload')
 
     upload.upload(uploader.get_max_image_size())
+
+    pkg = toolkit.get_action('package_update')(context, data_dict)
 
     return pkg
