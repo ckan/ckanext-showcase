689493f33b05385a642dfd526e6daa36ffd910fb
Brook Elgie <specialbrew@gmail.com>
2014-11-09 16:36:59 +0000

    Remove unneeded code from PackageController subclass

--- ckanext/showcase/controller.py
+++ ckanext/showcase/controller.py
@@ -1,19 +1,13 @@
 from pylons import config
 
-from genshi.template import MarkupTemplate
-
 import ckan.model as model
 import ckan.lib.base as base
 import ckan.lib.helpers as h
 import ckan.lib.navl.dictization_functions as dict_fns
-import ckan.lib.render
-import ckan.lib.package_saver as package_saver
 import ckan.logic as logic
-from ckan.common import c, request, _, response
+from ckan.common import c, request, _
 from ckan.controllers.package import PackageController
-from ckan.controllers.home import CACHE_PARAMETERS
 
-render = base.render
 abort = base.abort
 redirect = base.redirect
 NotFound = logic.NotFound
@@ -23,7 +17,6 @@ get_action = logic.get_action
 tuplize_dict = logic.tuplize_dict
 clean_dict = logic.clean_dict
 parse_params = logic.parse_params
-
 NotAuthorized = logic.NotAuthorized
 
 from ckanext.showcase.plugin import DATASET_TYPE_NAME
@@ -35,132 +28,43 @@ log = logging.getLogger(__name__)
 class ShowcaseController(PackageController):
 
     def new(self, data=None, errors=None, error_summary=None):
-        package_type = DATASET_TYPE_NAME
 
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'auth_user_obj': c.userobj,
                    'save': 'save' in request.params}
 
-        # Package needs to have a organization group in the call to
-        # check_access and also to save it
+        # Check access here, then continue with PackageController.new()
+        # PackageController.new will also check access for package_create.
+        # This is okay for now, while only sysadmins can create Showcases, but
+        # may not work if we allow other users to create Showcases, who don't
+        # have access to create dataset package types. Same for edit below.
         try:
             check_access('ckanext_showcase_create', context)
         except NotAuthorized:
             abort(401, _('Unauthorized to create a package'))
 
-        if context['save'] and not data:
-            return self._save_new(context, package_type=package_type)
-
-        data = data or clean_dict(dict_fns.unflatten(tuplize_dict(parse_params(
-            request.params, ignore_keys=CACHE_PARAMETERS))))
-        c.resources_json = h.json.dumps(data.get('resources', []))
-        # convert tags if not supplied in data
-        if data and not data.get('tag_string'):
-            data['tag_string'] = ', '.join(
-                h.dict_list_reduce(data.get('tags', {}), 'name'))
-
-        errors = errors or {}
-        error_summary = error_summary or {}
-        # in the phased add dataset we need to know that
-        # we have already completed stage 1
-        stage = ['active']
-        if data.get('state', '').startswith('draft'):
-            stage = ['active', 'complete']
-
-        form_snippet = self._package_form(package_type=package_type)
-        form_vars = {'data': data, 'errors': errors,
-                     'error_summary': error_summary,
-                     'action': 'new', 'stage': stage,
-                     'dataset_type': package_type}
-        c.errors_json = h.json.dumps(errors)
-
-        self._setup_template_variables(context, {},
-                                       package_type=package_type)
-
-        new_template = self._new_template(package_type)
-        c.form = ckan.lib.render.deprecated_lazy_render(
-            new_template,
-            form_snippet,
-            lambda: render(form_snippet, extra_vars=form_vars),
-            'use of c.form is deprecated. please see '
-            'ckan/templates/package/base_form_page.html for an example '
-            'of the new way to include the form snippet'
-            )
-        return render(new_template,
-                      extra_vars={'form_vars': form_vars,
-                                  'form_snippet': form_snippet,
-                                  'dataset_type': package_type})
+        return super(ShowcaseController, self).new(data=data, errors=errors,
+                                                   error_summary=error_summary)
 
     def edit(self, id, data=None, errors=None, error_summary=None):
-        package_type = DATASET_TYPE_NAME
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'auth_user_obj': c.userobj,
                    'save': 'save' in request.params,
                    'moderated': config.get('moderated'),
                    'pending': True}
 
-        if context['save'] and not data:
-            return self._save_edit(id, context, package_type=package_type)
-        try:
-            c.pkg_dict = get_action('package_show')(context, {'id': id})
-            context['for_edit'] = True
-            old_data = get_action('package_show')(context, {'id': id})
-            # old data is from the database and data is passed from the
-            # user if there is a validation error. Use users data if there.
-            if data:
-                old_data.update(data)
-            data = old_data
-        except NotAuthorized:
-            abort(401, _('Unauthorized to read package %s') % '')
-        except NotFound:
-            abort(404, _('Dataset not found'))
-        # are we doing a multiphase add?
-        if data.get('state', '').startswith('draft'):
-            c.form_action = h.url_for(controller='package', action='new')
-            c.form_style = 'new'
-            return self.new(data=data, errors=errors,
-                            error_summary=error_summary)
-
-        c.pkg = context.get("package")
-        c.resources_json = h.json.dumps(data.get('resources', []))
-
         try:
             check_access('ckanext_showcase_update', context)
         except NotAuthorized:
             abort(401, _('User %r not authorized to edit %s') % (c.user, id))
-        # convert tags if not supplied in data
-        if data and not data.get('tag_string'):
-            data['tag_string'] = ', '.join(h.dict_list_reduce(
-                c.pkg_dict.get('tags', {}), 'name'))
-        errors = errors or {}
-        form_snippet = self._package_form(package_type=package_type)
-        form_vars = {'data': data, 'errors': errors,
-                     'error_summary': error_summary, 'action': 'edit',
-                     'dataset_type': package_type}
-        c.errors_json = h.json.dumps(errors)
 
-        self._setup_template_variables(context, {'id': id},
-                                       package_type=package_type)
-        c.related_count = c.pkg.related_count
+        return super(ShowcaseController, self).edit(id, data=data, errors=errors,
+                                                    error_summary=error_summary)
 
-        # we have already completed stage 1
-        form_vars['stage'] = ['active']
-        if data.get('state', '').startswith('draft'):
-            form_vars['stage'] = ['active', 'complete']
+    def _guess_package_type(self, expecting_name=False):
+        """Showcase packages are always DATASET_TYPE_NAME."""
 
-        edit_template = self._edit_template(package_type)
-        c.form = ckan.lib.render.deprecated_lazy_render(
-            edit_template,
-            form_snippet,
-            lambda: render(form_snippet, extra_vars=form_vars),
-            'use of c.form is deprecated. please see '
-            'ckan/templates/package/edit.html for an example '
-            'of the new way to include the form snippet'
-            )
-        return render(edit_template,
-                      extra_vars={'form_vars': form_vars,
-                                  'form_snippet': form_snippet,
-                                  'dataset_type': package_type})
+        return DATASET_TYPE_NAME
 
     def _save_new(self, context, package_type=None):
         # The staged add dataset used the new functionality when the dataset is
@@ -286,84 +190,3 @@ class ShowcaseController(PackageController):
             errors = e.error_dict
             error_summary = e.error_summary
             return self.edit(name_or_id, data_dict, errors, error_summary)
-
-    # def read(self, id, format='html'):
-    #     if not format == 'html':
-    #         ctype, extension, loader = \
-    #             self._content_type_from_extension(format)
-    #         if not ctype:
-    #             # An unknown format, we'll carry on in case it is a
-    #             # revision specifier and re-constitute the original id
-    #             id = "%s.%s" % (id, format)
-    #             ctype, format, loader = "text/html; charset=utf-8", "html", \
-    #                 MarkupTemplate
-    #     else:
-    #         ctype, format, loader = self._content_type_from_accept()
-
-    #     response.headers['Content-Type'] = ctype
-
-    #     package_type = self._get_package_type(id.split('@')[0])
-    #     context = {'model': model, 'session': model.Session,
-    #                'user': c.user or c.author, 'for_view': True,
-    #                'auth_user_obj': c.userobj}
-    #     data_dict = {'id': id}
-
-    #     # interpret @<revision_id> or @<date> suffix
-    #     split = id.split('@')
-    #     if len(split) == 2:
-    #         data_dict['id'], revision_ref = split
-    #         if model.is_id(revision_ref):
-    #             context['revision_id'] = revision_ref
-    #         else:
-    #             try:
-    #                 date = h.date_str_to_datetime(revision_ref)
-    #                 context['revision_date'] = date
-    #             except TypeError, e:
-    #                 abort(400, _('Invalid revision format: %r') % e.args)
-    #             except ValueError, e:
-    #                 abort(400, _('Invalid revision format: %r') % e.args)
-    #     elif len(split) > 2:
-    #         abort(400, _('Invalid revision format: %r') %
-    #               'Too many "@" symbols')
-
-    #     # check if package exists
-    #     try:
-    #         c.pkg_dict = get_action('package_show')(context, data_dict)
-    #         c.pkg = context['package']
-    #     except NotFound:
-    #         abort(404, _('Dataset not found'))
-    #     except NotAuthorized:
-    #         abort(401, _('Unauthorized to read package %s') % id)
-
-    #     # used by disqus plugin
-    #     c.current_package_id = c.pkg.id
-    #     c.related_count = c.pkg.related_count
-
-    #     # can the resources be previewed?
-    #     for resource in c.pkg_dict['resources']:
-    #         # Backwards compatibility with preview interface
-    #         resource['can_be_previewed'] = self._resource_preview(
-    #             {'resource': resource, 'package': c.pkg_dict})
-
-    #         resource_views = get_action('resource_view_list')(
-    #             context, {'id': resource['id']})
-    #         resource['has_views'] = len(resource_views) > 0
-
-    #     self._setup_template_variables(context, {'id': id},
-    #                                    package_type=package_type)
-
-    #     package_saver.PackageSaver().render_package(c.pkg_dict, context)
-
-    #     template = self._read_template(package_type)
-    #     template = template[:template.index('.') + 1] + format
-
-    #     try:
-    #         return render(template, loader_class=loader,
-    #                       extra_vars={'dataset_type': package_type})
-    #     except ckan.lib.render.TemplateNotFound:
-    #         msg = _("Viewing {package_type} datasets in {format} format is "
-    #                 "not supported (template file {file} not found).".format(
-    #                     package_type=package_type, format=format, file=template))
-    #         abort(404, msg)
-
-    #     assert False, "We should never get here"
