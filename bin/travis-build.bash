bc36a9e63855c98f77a23e7e9e9333855eefeba8
Brook Elgie <specialbrew@gmail.com>
2014-11-26 13:31:50 +0000

    Travis just use ckan/master

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,7 +10,7 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout cc90797d44e9856c7b85f7a7eb0e4a355bae98fc
+git checkout master
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
