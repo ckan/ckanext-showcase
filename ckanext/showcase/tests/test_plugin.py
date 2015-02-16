ddede70f4fa93a66ae3f0b03b2f7352811411194
Brook Elgie <specialbrew@gmail.com>
2015-02-16 11:15:12 +0000

    Create showcase view tests

--- ckanext/showcase/tests/test_plugin.py
+++ ckanext/showcase/tests/test_plugin.py
@@ -1,3 +1,4 @@
+from routes import url_for
 from nose import tools as nosetools
 
 import ckan.new_tests.helpers as helpers
@@ -5,6 +6,8 @@ import ckan.new_tests.factories as factories
 
 import logging
 log = logging.getLogger(__name__)
+
+submit_and_follow = helpers.submit_and_follow
 
 
 class TestShowcaseIndex(helpers.FunctionalTestBase):
@@ -35,3 +38,37 @@ class TestShowcaseIndex(helpers.FunctionalTestBase):
         response = app.get("/showcase", status=200)
         response.mustcontain("1 showcase found")
         response.mustcontain("my-showcase")
+
+
+class TestShowcaseNewView(helpers.FunctionalTestBase):
+
+    def test_showcase_create_form_renders(self):
+        app = self._get_test_app()
+        sysadmin = factories.Sysadmin()
+
+        env = {'REMOTE_USER': sysadmin['name'].encode('ascii')}
+        response = app.get(
+            url=url_for(controller='ckanext.showcase.controller:ShowcaseController', action='new'),
+            extra_environ=env,
+        )
+        nosetools.assert_true('dataset-edit' in response.forms)
+
+    def test_showcase_new_redirects_to_add_datasets(self):
+        '''Creating a new showcase redirects to the add datasets form.'''
+        app = self._get_test_app()
+        sysadmin = factories.Sysadmin()
+        factories.Dataset(name='my-showcase')
+
+        env = {'REMOTE_USER': sysadmin['name'].encode('ascii')}
+        response = app.get(
+            url=url_for(controller='ckanext.showcase.controller:ShowcaseController', action='new'),
+            extra_environ=env,
+        )
+
+        # create showcase
+        form = response.forms['dataset-edit']
+        form['name'] = u'my-dataset'
+        create_response = submit_and_follow(app, form, env, 'save')
+
+        # Unique to add_datasets page
+        nosetools.assert_true('bulk_action.showcase_add' in create_response)
