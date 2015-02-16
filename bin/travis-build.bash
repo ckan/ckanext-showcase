ca98eb88f174c11869c6785b93e504c8c793bc50
Brook Elgie <specialbrew@gmail.com>
2015-02-16 11:55:25 +0000

    Travis use release branch

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,7 +10,7 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout master
+git checkout release-v2.3
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
