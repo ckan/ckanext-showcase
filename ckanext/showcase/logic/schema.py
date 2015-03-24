f97b1db2c7182b2c3e37ced023f52330e2e9774b
Brook Elgie <specialbrew@gmail.com>
2015-03-24 15:08:12 +0000

    Add original_related_item_id to showcase schema

--- ckanext/showcase/logic/schema.py
+++ ckanext/showcase/logic/schema.py
@@ -41,7 +41,9 @@ def showcase_base_schema():
         'save': [ignore],
         'return_to': [ignore],
         'image_url': [toolkit.get_validator('ignore_missing'),
-                      toolkit.get_converter('convert_to_extras')]
+                      toolkit.get_converter('convert_to_extras')],
+        'original_related_item_id': [toolkit.get_converter('ignore_missing'),
+                                     toolkit.get_converter('convert_to_extras')]
     }
     return schema
 
@@ -99,7 +101,9 @@ def showcase_show_schema():
 
     schema.update({
         'image_url': [toolkit.get_converter('convert_from_extras'),
-                      toolkit.get_validator('ignore_missing')]
+                      toolkit.get_validator('ignore_missing')],
+        'original_related_item_id': [toolkit.get_converter('convert_from_extras'),
+                                     toolkit.get_validator('ignore_missing')],
     })
 
     return schema
