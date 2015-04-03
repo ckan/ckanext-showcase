475f8b597b5e296d9324a96fddb954e0d3d7e675
Brook Elgie <specialbrew@gmail.com>
2015-04-03 14:22:24 +0100

    Tests need master to pass

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,7 +10,7 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout release-v2.3
+git checkout master
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
