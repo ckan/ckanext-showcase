902526c2717ba54835b3ab9edb6dd20e1d1315fe
Brook Elgie <specialbrew@gmail.com>
2016-04-21 11:45:13 +0100

    Change how travis pick which ckan branch to use

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,15 +10,7 @@ sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-if [ $CKANVERSION == '2.3' ]
-then
-    git checkout release-v2.3
-elif [ $CKANVERSION == '2.4.0' ]
-then
-    git checkout release-v2.4.0
-else
-    git checkout master
-fi
+git checkout $CKAN_BRANCH
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
@@ -35,6 +27,7 @@ cd -
 
 echo "Installing ckanext-showcase and its requirements..."
 python setup.py develop
+pip install -r requirements.txt
 pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
