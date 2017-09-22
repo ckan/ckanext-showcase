67cb9351d99df6638a081593a80d8ac4079302a3
Stefanie Taepke <stefanie.taepke@liip.ch>
2017-09-22 14:08:43 +0200

    fix travis build
    Analogue to https://github.com/ckan/ckanext-pages/pull/59/files

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,12 +5,14 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
+sudo apt-get install solr-jetty libcommons-fileupload-java
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-git checkout $CKAN_BRANCH
+export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
+echo "CKAN branch: $latest_ckan_release_branch"
+git checkout $latest_ckan_release_branch
 python setup.py develop
 pip install -r requirements.txt --allow-all-external
 pip install -r dev-requirements.txt --allow-all-external
