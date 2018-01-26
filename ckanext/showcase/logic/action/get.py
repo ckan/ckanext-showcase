63f4914e5aa11899afe791c102d5675da3fa6446
Stefanie Taepke <stefina@users.noreply.github.com>
2018-01-26 11:08:45 +0100

    change logging-level to debug

--- ckanext/showcase/logic/action/get.py
+++ ckanext/showcase/logic/action/get.py
@@ -124,7 +124,7 @@ def package_showcase_list(context, data_dict):
                 )
                 showcase_list.append(showcase)
             except NotAuthorized:
-                log.error('Not authorized to access Package with ID: '
+                log.debug('Not authorized to access Package with ID: '
                           + str(showcase_id))
     return showcase_list
 
