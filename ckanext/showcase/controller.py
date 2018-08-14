14a554310c9403278be599805ce84aeec900b937
roll <roll@users.noreply.github.com>
2018-08-14 17:25:30 +0300

    Added rich-text editing, iframe embedding, simplified datasets linking (#1)
    Rich-text editing, iframe embedding, simplified dataset linking

--- ckanext/showcase/controller.py
+++ ckanext/showcase/controller.py
@@ -80,8 +80,7 @@ class ShowcaseController(PackageController):
 
     def _save_new(self, context, package_type=None):
         '''
-        The showcase is created then redirects to the manage_dataset page to
-        associated packages with the new showcase.
+        The showcase is created then redirects to the showcase read page.
         '''
 
         data_dict = clean_dict(dict_fns.unflatten(
@@ -99,10 +98,10 @@ class ShowcaseController(PackageController):
             data_dict['state'] = 'none'
             return self.new(data_dict, errors, error_summary)
 
-        # redirect to manage datasets
+        # redirect to showcase details page
         url = h.url_for(
             controller='ckanext.showcase.controller:ShowcaseController',
-            action='manage_datasets', id=pkg_dict['name'])
+            action='read', id=pkg_dict['name'])
         redirect(url)
 
     def _save_edit(self, name_or_id, context, package_type=None):
@@ -203,8 +202,10 @@ class ShowcaseController(PackageController):
 
         try:
             c.pkg_dict = get_action('package_show')(context, data_dict)
-            c.showcase_list = get_action('ckanext_package_showcase_list')(
-                context, {'package_id': c.pkg_dict['id']})
+            c.showcase_list = get_action('package_search')(context, {
+                'q': 'dataset_names:%s' % c.pkg_dict['name'],
+                'fq': 'dataset_type:showcase',
+            })['results']
         except NotFound:
             abort(404, _('Dataset not found'))
         except logic.NotAuthorized:
