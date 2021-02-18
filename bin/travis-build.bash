d5a6bc617a100de028f171714d89c130b92858c0
pdelboca <patriciodelboca@gmail.com>
2021-02-18 12:54:15 -0300

    Travis - Use docker for solr

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,7 +5,6 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty
 
 if python -c 'import sys;exit(sys.version_info < (3,))'
 then
@@ -26,27 +25,27 @@ else
     echo "CKAN version: ${CKAN_TAG#ckan-}"
 fi
 
+echo "Installing the recommended setuptools requirement"
 if [ -f requirement-setuptools.txt ]
 then
     pip install -r requirement-setuptools.txt
 fi
+
 python setup.py develop
 
 if [ -f requirements-py2.txt ] && [ $PYTHONVERSION = 2 ]
 then
-    grep -v psycopg2 < requirements-py2.txt > reqs.txt
+    pip install -r requirements-py2.txt
 else
-    grep -v psycopg2 < requirements.txt > reqs.txt
+    pip install -r requirements.txt
 fi
-pip install psycopg2==2.7.7  # workaround travis 10 psycopg2 incompatibility
-pip install -r reqs.txt
+
 pip install -r dev-requirements.txt
+
 cd -
 
 echo "Setting up Solr..."
-printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
-sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
+docker run --name ckan-solr -p 8983:8983 -d openknowledge/ckan-solr-dev:$CKANVERSION
 
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
@@ -66,10 +65,6 @@ echo "Installing extension and its requirements..."
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
 python setup.py develop
-
-echo "Updating solr_url to single core"
-sed -i -e 's/use = config:..\/ckan\/test-core.ini/use = config:..\/ckan\/test-core.ini\nsolr_url = http:\/\/127.0.0.1:8983\/solr/' test*.ini
-
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
